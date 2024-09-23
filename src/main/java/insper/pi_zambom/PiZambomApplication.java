91303d5d8b9639b6c6ee0399ed5b1d1193c39897
Benibergas <benis@al.insper.edu.br>
2024-09-23 11:24:14 -0300

    7

--- src/main/java/insper/pi_zambom/PiZambomApplication.java
+++ src/main/java/insper/pi_zambom/PiZambomApplication.java
@@ -2,19 +2,18 @@ package insper.pi_zambom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@RestController
 public class PiZambomApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiZambomApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String hello() {
-		return "Hello World!";
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
